968edb844541d74ca4d7ecd38da6617f418b6313
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-05 18:07:43 -0500

    commit tree 4ed4037c34c84081df7dacc98ceb195b2f5c833d

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+    start = 2
+    if n > 1
+        range = (start..n-1).to_a
+        range.none? { |integer| n % integer == 0 }
+    # end
+    else 
+        false
+    end
+
+end
